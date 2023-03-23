7246e58888aa0430d7f152051a00f3bb73b9c4d6
JHusayan <joshua.alex.husayan@gmail.com>
2023-03-23 14:54:34 +0800

    Adjusted work cards

--- src/pages/components/work.tsx
+++ src/pages/components/work.tsx
@@ -9,17 +9,17 @@ const Work = ({workRef}:any) => {
   return (
     <div className='bg-neutral-900 h-[500px] text-white flex flex-col space-x-8 justify-center px-[10%] my-[2%] items-center pb-3' ref={workRef}>
       <h1 className="text-8xl font-medium mb-6 leading-none tracking-tight text-white md:text-5xl lg:text-6xl dark:text-white select-none">Work</h1>
-      <div className='flex flex-row space-x-8 justify-center p-5 items-center'>
-        <Card>
-          <img className='object-cover' src={DNALogo} alt="DNA Logo"/>
+      <div className='flex justify-evenly space-x-12 p-5 flex-col md:flex-row'>
+        <Card className='h-64 w-64 min-w-full'>
+          <img className='h-64 w-64 object-contain' src={DNALogo} alt="DNA Logo"/>
           <div className="font-bold text-xl mb-2 select-none">Junior Software Developer</div>
         </Card>
-        <Card>
-          <img className='object-cover' src={SymphLogo} alt="Symph Logo"/>
+        <Card className='h-64 w-64 min-w-full'>
+          <img className='h-64 w-full object-contain' src={SymphLogo} alt="Symph Logo"/>
           <div className="font-bold text-xl mb-2 select-none">QA Technical Intern</div>       
         </Card>
-        <Card>
-          <img className='object-cover' src={USCLogo} alt="USC Logo"/>
+        <Card className='h-64 w-64 min-w-full'>
+          <img className='h-64 w-64 object-cover' src={USCLogo} alt="USC Logo"/>
           <div className="font-bold text-xl mb-2 select-none">Thesis Project Manager</div>      
         </Card>
       </div>
